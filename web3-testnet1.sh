9336b971ca63748efd6b03a9a4e2a10f39d5677b
Deric Walintukan <deric.walintukan@gmail.com>
2018-11-27 15:05:30 +0700

    Change container name

--- web3-testnet1.sh
+++ web3-testnet1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker exec -it naka-server_testnet-node1 /usr/local/bin/geth attach ipc:/root/.naka/geth.ipc
+docker exec -it testnet-node1 /usr/local/bin/geth attach ipc:/root/.naka/geth.ipc
