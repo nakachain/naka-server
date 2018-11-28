fa78c0f5c5350a119b32989cd6254a6dde1cea07
Deric Walintukan <deric.walintukan@gmail.com>
2018-11-28 18:35:00 +0700

    Add ws capability for client servers

--- client/start-client.sh
+++ client/start-client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-geth --datadir /root/.naka --syncmode full --networkid 25 --nat=none --targetgaslimit 4700000 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --verbosity 4 --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:30301"
+geth --datadir /root/.naka --syncmode full --networkid 25 --nat=none --targetgaslimit 4700000 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ws --wsaddr 0.0.0.0 --verbosity 4 --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:30301"
