c06a2a58dcd87191aa71f2566ed98fcead7a5292
Deric Walintukan <deric.walintukan@gmail.com>
2018-12-05 11:52:04 +0700

    Enable ws apis

--- client/start-client.sh
+++ client/start-client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-geth --datadir /root/.naka --syncmode full --gcmode archive --networkid 25 --nat=none --targetgaslimit 4700000 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi db,debug,eth,net,web3 --ws --wsaddr 0.0.0.0 --verbosity 4 --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:30301"
+geth --datadir /root/.naka --syncmode full --gcmode archive --networkid 25 --nat=none --targetgaslimit 4700000 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi db,debug,eth,net,web3 --ws --wsaddr 0.0.0.0 --wsapi db,debug,eth,net,web3 --verbosity 4 --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:30301"
