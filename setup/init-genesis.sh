fd315ab74204730dbcfa77692af3c12586a7ed40
Deric Walintukan <deric.walintukan@gmail.com>
2018-12-09 23:41:37 +0700

    Always call init on genesis

--- setup/init-genesis.sh
+++ setup/init-genesis.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-DIRECTORY="/root/.naka/geth"
+# DIRECTORY="/root/.naka/geth"
 
-if [ ! -d "$DIRECTORY" ]; then
+# if [ ! -d "$DIRECTORY" ]; then
     # Control will enter here if $DIRECTORY doesn't exist.
     geth --datadir /root/.naka init /root/.naka/genesis.json
-fi
+# fi
