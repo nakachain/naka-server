e3868ff93ea6890999570ba955df0bcf5cf34179
Deric Walintukan <deric.walintukan@gmail.com>
2019-01-07 16:00:19 -0800

    Refactor client compose and scripts

--- client/start-client.sh
+++ client/start-client.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-geth --datadir /root/.naka --syncmode full --gcmode archive --networkid 25 --nat=none --targetgaslimit 4700000 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcapi db,debug,eth,net,web3 --ws --wsaddr 0.0.0.0 --wsorigins "*" --wsapi db,debug,eth,net,web3 --verbosity 4 --bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:30301"
+geth \
+--datadir /root/.naka \
+--syncmode full \
+--gcmode archive \
+--networkid "$CHAIN_ID" \
+--nat=none \
+--targetgaslimit 4700000 \
+--rpc \
+--rpcaddr 0.0.0.0 \
+--rpccorsdomain "*" \
+--rpcapi db,debug,eth,net,web3 \
+--ws \
+--wsaddr 0.0.0.0 \
+--wsorigins "*" \
+--wsapi db,debug,eth,net,web3 \
+--verbosity 4 \
+--bootnodes "enode://$BOOTNODE_ID@$BOOTNODE_IP:30301"
