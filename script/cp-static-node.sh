29ff6c1e6ded90bbc5c9f2772a1f0f1763720014
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-21 21:41:12 +0900

    Use nested ifs

--- script/cp-static-node.sh
+++ script/cp-static-node.sh
@@ -1,6 +1,18 @@
 # Copy static-node.json to Docker volume
 #!/bin/sh
 
+MESSAGE="$ ./cp-static-node.sh [mainnet/testnet] [sealer/client]"
+MAINNET_SEALER_VOL_NAME="mainnet_mainnet-sealer-data"
+MAINNET_CLIENT_VOL_NAME="mainnet_mainnet-client-data"
+TESTNET_SEALER_VOL_NAME="testnet_testnet-sealer-data"
+TESTNET_CLIENT_VOL_NAME="testnet_testnet-client-data"
+
+display_error_msg () {
+    echo "invalid network or node type"
+    echo ${MESSAGE}
+    exit 2
+}
+
 # Get mountpoint path
 get_mountpoint () {
     docker volume inspect $1 > volume.txt
@@ -8,31 +20,33 @@ get_mountpoint () {
     rm volume.txt
 }
 
-MESSAGE="$ ./cp-static-node.sh [mainnet/testnet] [sealer/client]"
-MAINNET_SEALER_VOL_NAME="mainnet_mainnet-sealer-data"
-MAINNET_CLIENT_VOL_NAME="mainnet_mainnet-client-data"
-TESTNET_SEALER_VOL_NAME="testnet_testnet-sealer-data"
-TESTNET_CLIENT_VOL_NAME="testnet_testnet-client-data"
-
-echo "Copying static-node.json"
-
 # Validate network
-if [ $1 = "mainnet" -a $2 = "sealer" ] then
-    get_mountpoint $MAINNET_SEALER_VOL_NAME
-    sudo cp -r ./mainnet/static-nodes.json $MOUNTPOINT
-elif [ $1 = "mainnet" -a $2 = "client" ] then
-    get_mountpoint $MAINNET_CLIENT_VOL_NAME
-    sudo cp -r ./mainnet/static-nodes.json $MOUNTPOINT
-elif [ $1 = "testnet" -a $2 = "sealer" ] then
-    get_mountpoint $TESTNET_SEALER_VOL_NAME
-    sudo cp -r ./testnet/static-nodes.json $MOUNTPOINT
-elif [ $1 = "testnet" -a $2 = "client" ] then
-    get_mountpoint $TESTNET_CLIENT_VOL_NAME
-    sudo cp -r ./testnet/static-nodes.json $MOUNTPOINT
+if [ $1 = "mainnet" ]; then
+    if [ $2 = "sealer" ]; then
+        echo "Copying static-node.json to mainnet sealer"
+        get_mountpoint $MAINNET_SEALER_VOL_NAME
+        sudo cp -r ./mainnet/static-nodes.json $MOUNTPOINT
+    elif [ $2 = "client" ]; then
+        echo "Copying static-node.json to mainnet client"
+        get_mountpoint $MAINNET_CLIENT_VOL_NAME
+        sudo cp -r ./mainnet/static-nodes.json $MOUNTPOINT
+    else
+        display_error_msg
+    fi
+elif [ $1 = "testnet" ]; then
+    if [ $2 = "sealer" ]; then
+        echo "Copying static-node.json to testnet sealer"
+        get_mountpoint $TESTNET_SEALER_VOL_NAME
+        sudo cp -r ./testnet/static-nodes.json $MOUNTPOINT
+    elif [ $2 = "client" ]; then
+        echo "Copying static-node.json to testnet client"
+        get_mountpoint $TESTNET_CLIENT_VOL_NAME
+        sudo cp -r ./testnet/static-nodes.json $MOUNTPOINT
+    else
+        display_error_msg
+    fi
 else
-    echo "invalid network or node type"
-    echo ${MESSAGE}
-    exit 2
+    display_error_msg
 fi
 
 echo "Success"
