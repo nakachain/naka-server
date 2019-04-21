09b2f19920f56a41241d00321f8ee71e07661d24
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-22 00:15:01 +0900

    Static nodes (#16)
    * Add cp static node script
    
    * Add set of brackets
    
    * Remove semi colons
    
    * Use -a flag
    
    * Use nested ifs
    
    * Remove semicolons
    
    * Use double equals;
    
    * Use quotes around var
    
    * Fix volume names
    
    * Fix path
    
    * Rename node to sealer folder
    
    * Change path for static nodes json
    
    * Copy static node in dockerfiles
    
    * Only init if datadir does not exist
    
    * Add echo
    
    * Remove quotes from dir check
    
    * Disable init genesis
    
    * Fix bad ref to old node folder
    
    * Turn off discovery
    
    * Remove nodiscover
    
    * Add restart policy to composes
    
    * Fix compose restart policy
    
    * Use restart instead of deploy restart policy
    
    * Fix static node typo

---
+++ script/cp-static-node.sh
@@ -0,0 +1,52 @@
+# Copy static-node.json to Docker volume
+#!/bin/sh
+
+MESSAGE="$ ./cp-static-node.sh [mainnet/testnet] [sealer/client]"
+MAINNET_SEALER_VOL_NAME="sealer_mainnet-sealer-data"
+MAINNET_CLIENT_VOL_NAME="client_mainnet-client-data"
+TESTNET_SEALER_VOL_NAME="sealer_testnet-sealer-data"
+TESTNET_CLIENT_VOL_NAME="client_testnet-client-data"
+
+display_error_msg () {
+    echo "invalid network or node type"
+    echo ${MESSAGE}
+    exit 2
+}
+
+# Get mountpoint path
+get_mountpoint () {
+    docker volume inspect $1 > volume.txt
+    MOUNTPOINT="$(jq -r '.[] | .Mountpoint' volume.txt)"
+    rm volume.txt
+}
+
+# Validate network
+if [ "$1" = "mainnet" ]; then
+    if [ "$2" = "sealer" ]; then
+        echo "Copying static-node.json to mainnet sealer"
+        get_mountpoint $MAINNET_SEALER_VOL_NAME
+        sudo cp -r ../mainnet/static-nodes.json "${MOUNTPOINT}/geth"
+    elif [ "$2" = "client" ]; then
+        echo "Copying static-node.json to mainnet client"
+        get_mountpoint $MAINNET_CLIENT_VOL_NAME
+        sudo cp -r ../mainnet/static-nodes.json "${MOUNTPOINT}/geth"
+    else
+        display_error_msg
+    fi
+elif [ "$1" = "testnet" ]; then
+    if [ "$2" = "sealer" ]; then
+        echo "Copying static-node.json to testnet sealer"
+        get_mountpoint $TESTNET_SEALER_VOL_NAME
+        sudo cp -r ../testnet/static-nodes.json "${MOUNTPOINT}/geth"
+    elif [ "$2" = "client" ]; then
+        echo "Copying static-node.json to testnet client"
+        get_mountpoint $TESTNET_CLIENT_VOL_NAME
+        sudo cp -r ../testnet/static-nodes.json "${MOUNTPOINT}/geth"
+    else
+        display_error_msg
+    fi
+else
+    display_error_msg
+fi
+
+echo "Success"
--- script/init-genesis.sh
+++ script/init-genesis.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# DIRECTORY="/root/.naka/geth"
+DIRECTORY=/root/.naka/geth
 
-# if [ ! -d "$DIRECTORY" ]; then
-    # Control will enter here if $DIRECTORY doesn't exist.
+if [ ! -d "$DIRECTORY" ]; then
+    echo "Datadir not found, exec geth init"
     geth --datadir /root/.naka init /root/.naka/genesis.json
-# fi
+fi
