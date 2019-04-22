540c83d767262f7422fe97ae122e5d3c98b58e83
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-22 15:50:55 +0900

    Use bootnode bin

--- script/download-bin.sh
+++ script/download-bin.sh
@@ -2,18 +2,11 @@
 
 CURRENT_VERSION=v1.5.0
 
-# Install unzip
-echo "Installing unzip"
-sudo apt-get install unzip
-
 # Fetch and unzip bootnode
 echo "Downloading bootnode"
-wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VERSION/bootnode.zip"
-echo "Unzipping bootnode"
-unzip bootnode.zip
+wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VERSION/bootnode"
 chmod 755 bootnode
 sudo mv bootnode /usr/local/bin
-rm bootnode.zip
 
 # Fetch and unzip geth
 echo "Downloading geth"
@@ -21,4 +14,3 @@ wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VE
 mv geth-linux-amd64 geth
 chmod 755 geth
 sudo mv geth /usr/local/bin
-rm geth.zip
