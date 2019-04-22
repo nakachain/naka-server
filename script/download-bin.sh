fcb9ca9325551c26a8f0e42003bff1fc25b1a94c
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-22 15:36:55 +0900

    Add proper flag for unzip

--- script/download-bin.sh
+++ script/download-bin.sh
@@ -3,13 +3,14 @@
 CURRENT_VERSION=v1.5.0
 
 # Install unzip
+echo "Installing unzip"
 sudo apt-get install unzip
 
 # Fetch and unzip bootnode
 echo "Downloading bootnode"
 wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VERSION/bootnode.zip"
 echo "Unzipping bootnode"
-sudo unzip bootnode.zip /usr/local/bin/bootnode
+sudo unzip -o bootnode.zip /usr/local/bin
 sudo chmod 755 /usr/local/bin/bootnode
 rm bootnode.zip
 
@@ -17,6 +18,6 @@ rm bootnode.zip
 echo "Downloading geth"
 wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VERSION/geth.zip"
 echo "Unzipping geth"
-sudo unzip geth.zip /usr/local/bin/geth
+sudo unzip -o geth.zip /usr/local/bin
 sudo chmod 755 /usr/local/bin/geth
 rm geth.zip
