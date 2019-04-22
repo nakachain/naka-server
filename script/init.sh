2a46952e9ff859c1765f8bd52077b2629d308232
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-22 12:23:18 +0900

    Add init script

---
+++ script/init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Initializes a new node
+# ./init-account.sh $GENESIS_FILE $PW_FILE $PRIVKEY_FILE $STATICNODE_FILE
+
+$DATADIR="${HOME}/.naka"
+
+# Create genesis block
+geth --datadir $DATADIR init $1
+
+# Imports to the account to the datadir
+geth --datadir $DATADIR account import --password $2 $3
+
+# Copy static-nodes.json to data dir
+cp $4 "${DATADIR}/geth"
