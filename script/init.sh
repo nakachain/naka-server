668fd833b38958da320799dfbba6f030023f1066
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-22 12:52:48 +0900

    Add quotes around data dir usage

--- script/init.sh
+++ script/init.sh
@@ -5,13 +5,13 @@
 $DATADIR=~/.naka
 
 # Make dir if needed
-mkdir $DATADIR
+mkdir "$DATADIR"
 
 # Create genesis block
-geth --datadir $DATADIR init $1
+geth --datadir "$DATADIR" init $1
 
 # Imports to the account to the datadir
-geth --datadir $DATADIR account import --password $2 $3
+geth --datadir "$DATADIR" account import --password $2 $3
 
 # Copy static-nodes.json to data dir
 cp $4 "${DATADIR}/geth"
