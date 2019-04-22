57566773823960dcc1437c74a19ae67e16090df8
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-22 15:32:39 +0900

    Add unzip install

--- script/download-bin.sh
+++ script/download-bin.sh
@@ -2,14 +2,21 @@
 
 CURRENT_VERSION=v1.5.0
 
+# Install unzip
+sudo apt-get install unzip
+
+# Fetch and unzip bootnode
 echo "Downloading bootnode"
 wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VERSION/bootnode.zip"
 echo "Unzipping bootnode"
 unzip bootnode.zip /usr/local/bin
 sudo chmod 755 /usr/local/bin/bootnode
+rm bootnode.zip
 
+# Fetch and unzip geth
 echo "Downloading geth"
 wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VERSION/geth.zip"
 echo "Unzipping geth"
 unzip geth.zip /usr/local/bin
 sudo chmod 755 /usr/local/bin/geth
+rm geth.zip
