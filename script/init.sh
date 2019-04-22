16abbb07db9d1892c90e966b6e7b41cebd38ef03
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-22 14:17:17 +0900

    Fix init script

--- script/init.sh
+++ script/init.sh
@@ -2,7 +2,7 @@
 # Initializes a new node
 # ./init-account.sh $GENESIS_FILE $PW_FILE $PRIVKEY_FILE $STATICNODE_FILE
 
-$DATADIR="$HOME/.naka"
+$DATADIR=$HOME/.naka
 
 # Make dir if needed
 mkdir "$DATADIR"
@@ -14,4 +14,4 @@ geth --datadir "$DATADIR" init $1
 geth --datadir "$DATADIR" account import --password $2 $3
 
 # Copy static-nodes.json to data dir
-cp $4 "${DATADIR}/geth"
+cp $4 "$DATADIR/geth"
