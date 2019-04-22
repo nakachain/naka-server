4c81ccb257a13efdf1ee44ac20ab7e9975411d00
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-22 15:33:47 +0900

    Add sudo for unzipping

--- script/download-bin.sh
+++ script/download-bin.sh
@@ -9,7 +9,7 @@ sudo apt-get install unzip
 echo "Downloading bootnode"
 wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VERSION/bootnode.zip"
 echo "Unzipping bootnode"
-unzip bootnode.zip /usr/local/bin
+sudo unzip bootnode.zip /usr/local/bin
 sudo chmod 755 /usr/local/bin/bootnode
 rm bootnode.zip
 
@@ -17,6 +17,6 @@ rm bootnode.zip
 echo "Downloading geth"
 wget "https://github.com/nakachain/go-naka-release/releases/download/$CURRENT_VERSION/geth.zip"
 echo "Unzipping geth"
-unzip geth.zip /usr/local/bin
+sudo unzip geth.zip /usr/local/bin
 sudo chmod 755 /usr/local/bin/geth
 rm geth.zip
