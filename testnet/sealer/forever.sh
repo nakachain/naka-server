640c2e6e76e4e818d3b3bd07a8db128154a79737
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-29 11:37:28 +0700

    Inline start to forever script

--- testnet/sealer/forever.sh
+++ testnet/sealer/forever.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-until start.sh; do
-    echo "Server crashed with exit code $?. Respawning..." >&2
+while [ true ]
+do
+    export $(cat .env | xargs) && ../../node-sealer/start-node-nohup.sh
     sleep 1
 done
