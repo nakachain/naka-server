eb14636384cf21c0532f2f06397c79a9a4becabe
Deric Walintukan <deric.walintukan@gmail.com>
2019-04-29 14:25:42 +0700

    Add log init and system service init to script

--- script/init.sh
+++ script/init.sh
@@ -1,33 +1,44 @@
 #!/bin/sh
 # Initializes a new node
-# Inject env args before calling script
+# Must inject env args before calling script
 
-# Make data dir if needed
+# Setup data dir
 if [ ! -z "$DATA_DIR" ] && [ ! -d "$DATA_DIR" ]; then
-    echo "Creating data dir"
+    echo "Setting up data dir..."
     mkdir -p "$DATA_DIR"
 fi
 
-# Make logs dir if needed
+# Setup logging
 if [ ! -z "$LOG_DIR" ] && [ ! -d "$LOG_DIR" ]; then
-    echo "Creating log dir"
-    mkdir -p "$LOG_DIR"
+    echo "Setting up logging..."
+    sudo mkdir -p "$LOG_DIR"
+
+    # Route geth syslogs to own log file
+    sudo cp geth.conf /etc/rsyslog.d
+    sudo chown syslog:adm "$LOG_DIR/geth.log"
 fi
 
 # Create genesis block
 if [ ! -z "$DATA_DIR" ] && [ ! -z "$GENESIS_FILE" ] && [ ! -d "$DATA_DIR/geth/chaindata" ]; then
-    echo "Init genesis block"
+    echo "Init genesis block..."
     geth --datadir $DATA_DIR init $GENESIS_FILE
 fi
 
 # Imports to the account to the datadir
 if [ ! -z "$DATA_DIR" ] && [ ! -z "$PW_FILE" ] && [ ! -z "$PRIV_KEY_FILE" ]; then
-    echo "Importing account"
+    echo "Importing account..."
     geth --datadir "$DATA_DIR" account import --password "$PW_FILE" "$PRIV_KEY_FILE"
 fi
 
 # Copy static-nodes.json to data dir
 if [ ! -z "$STATIC_NODE_FILE" ] && [ ! -z "$DATA_DIR" ]; then
-    echo "Copying static-node.json"
+    echo "Copying static-node.json..."
     cp "$STATIC_NODE_FILE" "$DATA_DIR/geth"
+fi
+
+# Create geth systemd service
+if [ ! -f "$SERVICE_FILE" ]; then
+    echo "Creating geth service..."
+    sudo cp "$SERVICE_FILE" /etc/systemd/system 
+    sudo systemctl enable geth.service
 fi
