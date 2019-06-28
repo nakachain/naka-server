77fb4a2446b0e8c0123ecd639993f59bcefdfd47
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-28 16:08:08 +0700

    Add reader for attach

--- scripts/attach.sh
+++ scripts/attach.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 # Attaches geth console to instance
 
-MESSAGE="$ ./attach.sh [mainnet/testnet]"
-NETWORK=$1
+MAINNET_IPC=$HOME/.naka/mainnet/geth.ipc
+TESTNET_IPC=$HOME/.naka/testnet/geth.ipc
 
-if [ "$NETWORK" = "mainnet" ]; then
-    geth attach ipc:$HOME/.naka/mainnet/geth.ipc
-elif [ "$NETWORK" = "testnet" ]; then
-    geth attach ipc:$HOME/.naka/testnet/geth.ipc
+echo "Attaching geth"
+echo "=============="
+echo "Enter input:"
+echo "1 for Mainnet"
+echo "2 for Testnet"
+
+read NETWORK
+if [ "$NETWORK" = "1" ]; then
+    geth attach ipc:$MAINNET_IPC
+elif [ "$NETWORK" = "2" ]; then
+    geth attach ipc:$TESTNET_IPC
 else
     echo "Invalid network!"
-    echo "$MESSAGE"
 fi
