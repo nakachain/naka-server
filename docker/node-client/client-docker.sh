53970eb60f13614cb94e1505d102bc3c3095c841
Deric Walintukan <deric.walintukan@gmail.com>
2019-07-17 13:23:42 +0800

    Use non-docker script for docker

--- docker/node-client/client-docker.sh
+++ docker/node-client/client-docker.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 geth \
---datadir /root/.naka \
+--datadir "$DATA_DIR" \
 --syncmode full \
 --gcmode archive \
 --networkid "$CHAIN_ID" \
 --nat=none \
 --targetgaslimit 4700000 \
+--port "$LISTEN_PORT" \
 --rpc \
 --rpcaddr 0.0.0.0 \
+--rpcport "$RPC_PORT" \
 --rpccorsdomain "*" \
 --rpcapi db,debug,eth,net,web3 \
 --ws \
 --wsaddr 0.0.0.0 \
+--wsport "$WS_PORT" \
 --wsorigins "*" \
 --wsapi db,debug,eth,net,web3 \
 --verbosity 4 \
